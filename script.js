44883a435f716feb0ff9b467709e812f896e5898
WillianSmk <willian.smk@gmail.com>
2023-01-16 19:02:01 -0300

    add readme

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ function toggleMode() {
 
   // pegar a tag img
   const img = document.querySelector("#profile img")
+
   // subistituir a imagem
   if (html.classList.contains("light")) {
     //se tiver ligth mode, adcionar a imagem light
+
     img.setAttribute("src", "./assets/avatar-light.png")
   } else {
     // se tiver sem light mode, manter a imagem normal
